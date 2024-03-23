4993e086edce536a41beef7767e6737f5243c07f
Gnanaprakash J <gnanaprakash.j@games24x7.com>
2024-03-23 15:29:21 +0530

    added linkedlist exercises

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,5 +1,6 @@
 package linkedlist;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class LinkedList<T> {
@@ -95,6 +96,88 @@ public class LinkedList<T> {
 
     public int size(){
         return size;
+    }
+
+    public void reverse(){
+        if(isEmpty()) return;
+
+        Node p = null;
+        var c=first;
+        Node n;
+
+        while (c.next!=null){
+            n=c.next;
+            c.next=p;
+            p=c;
+            c=n;
+        }
+        last=first;
+        c.next=p;
+        first=c;
+    }
+
+    public int getKthNodeFromEnd(int k){
+        if(isEmpty())
+            throw new IllegalStateException();
+
+        var a=first;
+        var b=first;
+
+        for(int i=0;i<k-1;i++){
+            b=b.next;
+            if(b==null)
+                throw new IllegalArgumentException();
+        }
+
+        while (b!=last){
+            a=a.next;
+            b=b.next;
+        }
+
+        return a.item;
+    }
+
+    public void printMiddle(){
+        if(isEmpty())
+            throw new IllegalStateException();
+
+        var a=first;
+        var b=first;
+
+        //10 20 30
+
+        while (b!=last && b.next!=last){
+            b=b.next.next;
+            a=a.next;
+        }
+
+        if(b==last)
+            System.out.println(a.item);
+        else
+            System.out.println(a.item+","+a.next.item);
+    }
+
+    public boolean hasLoop(){
+        LinkedList list1=new LinkedList();
+        list1.addLast(10);
+        list1.addLast(20);
+        list1.addLast(30);
+        list1.addLast(40);
+        list1.addLast(50);
+//        list1.last.next=list1.first.next.next;
+
+        var slow=list1.first;
+        var fast=list1.first;
+
+        while (fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+
+            if(fast==slow)
+                return true;
+        }
+
+        return false;
     }
 
     public int[] toArray(){
